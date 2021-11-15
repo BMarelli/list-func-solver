1ab1744cd0cc3753c622c274d1353c89211cf425
BMarelli <marellibautista@gmail.com>
2021-11-15 16:55:48 -0300

    clean code

--- src/ListEval.hs
+++ src/ListEval.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE TypeApplications, ScopedTypeVariables #-}
 
 module ListEval where
-import Debug.Trace
 import AST
 import Data.Map.Strict as M hiding (splitAt, delete, map)
 import Data.Maybe
@@ -30,25 +29,18 @@ aplicar' fs l = aplicar_ fs (List.FList.fromList l)
       aplicar_ :: forall l m . (FList l, MonadState m, MonadError m) => [Funcs] -> l Elements -> m (l Elements)
       aplicar_ [] l = return l
       aplicar_ ((Zero or):fs) l = do let l' = zero or l
-                                    --  traceM ("zero " ++ show or ++ " -> " ++ printFL l')
                                      aplicar_ fs l'
       aplicar_ ((Succ or):fs) l = case succesor or l of
                                   Left err -> throw err
                                   Right l' -> aplicar_ fs l'
-                                  -- Right l' -> traceM ("succ " ++ show or ++ " -> " ++ printFL l') >> aplicar_ fs l'
       aplicar_ ((Delete or):fs) l = case delete or l of
                                   Left err -> throw err
-                                  Right l' -> aplicar_ fs l'
-                                  -- Right l' -> traceM ("delete " ++ show or ++ " -> " ++ printFL l') >> aplicar_ fs l'
+                                  Right l' -> aplicar_ fs l
       aplicar_ ((Rep f):fs) l = case rep f l of
                                   Left err -> throw err
-                                  Right l' -> aplicar_ fs l'
-                                  -- Right l' -> traceM ("rep " ++ " -> " ++ printFL l') >> aplicar_ fs l'
+                                  Right l' -> aplicar_ fs l
       aplicar_ ((Defined ss):fs) l = do fns <- look4func ss
                                         aplicar_ (fns ++ fs) l
-      -- aplicar_ ((Power fns 0):fs) l = aplicar_ fs l
-      -- aplicar_ ((Power fns n):fs) l = do l' <- aplicar_ fns l
-      --                                    aplicar_ ((Power fns (n-1)):fs) l'
 
 -- Dada una lista de funciones, una lista de elementos y un instancia de FList
 -- Aplica las funciones a la lista de elementos utilizando la instancia de FList
@@ -95,9 +87,6 @@ evalFunc ((Defined ss):fns) = do fs <- look4func ss
 evalFunc ((Rep fs):fns) = do fns' <- evalFunc fns
                              fs' <- evalFunc fs
                              return $ (Rep fs'):fns'
--- evalFunc ((Power fs n):fns) = do fns' <- evalFunc fns
---                                  fs' <- evalFunc fs
---                                  return $ (Power fs' n):fns'
 evalFunc (f:fns) = do fns' <- evalFunc fns
                       return $ f:fns'
 
@@ -117,9 +106,6 @@ inferExp (Term ((Delete _):fs) exp) n i = inferExp (Term fs exp) n (i-1)
 inferExp (Term ((Rep fns):fs) exp) n _ = throw InferRep
 inferExp (Term ((Defined ss):fs) exp) n i = do f <- look4func ss
                                                inferExp (Term (f++fs) exp) n i
--- inferExp (Term ((Power fns 0):fs) exp) n i = inferExp (Term fs exp) n i
--- inferExp (Term ((Power fns k):fs) exp) n i = throw InferPower 
-
 
 -- Evalua los distintos comandos y devuelve una TypedList como resultado
 evalComms :: (MonadState m, MonadError m) => Comms -> m (Maybe TypedList)
