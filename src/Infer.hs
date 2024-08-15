1c61836b154dd4e04a783b41dd8c627223c825a9
Bautista Marelli <marellibautista@gmail.com>
2024-08-14 22:52:26 -0300

    fix: `let in` infer calculation.

--- src/Infer.hs
+++ src/Infer.hs
@@ -4,17 +4,17 @@ import Data.Foldable (foldlM)
 import Lang
 import MonadFL (MonadFL, failFL, lookUpExp, lookUpFunc)
 import PPrint (ppError)
+import Subst (subst)
 
 infer :: (MonadFL m) => Int -> Exp Funcs Var -> m Int
 infer i (Const l) = return (i + length l)
 infer i (V var) =
   case var of
     Global name -> lookUpExp name >>= maybe (ppError ("Variable not found: " ++ name)) (infer i)
-    (Bound _) -> return i
     _ -> failFL "error: free variable"
 infer i (App fs e _) = foldlM inferFunc i fs >>= (`infer` e)
 infer i (Print e) = infer i e
-infer i (LetIn _ u v) = infer i u >>= (`infer` v)
+infer i (LetIn _ u v) = infer i (subst u v)
 
 inferFunc :: (MonadFL m) => Int -> Funcs -> m Int
 inferFunc i (Zero _) = return (i + 1)
